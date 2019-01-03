04c825f28905ccbd9ac3ca378c68833baecbcbb1
Plesca Sergiu <44344305+PlescaSergiu@users.noreply.github.com>
2019-01-03 22:15:16 +0200

    first commit

---
+++ src/athlete/Main.java
@@ -0,0 +1,12 @@
+package athlete;
+
+public class Main {
+
+    public static void main(String...args){
+        Athlete athlete = new Athlete();
+        athlete.readFromFile();
+        athlete.getLista();
+
+    }
+
+}
